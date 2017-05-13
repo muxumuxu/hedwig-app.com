c392dc0857a7fcadc5da0e43ec9d70e7d10f1157
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-05-13 19:22:03 +0200

    added iphone device and text

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,14 +1,12 @@
 // This is where it all goes :)
+
+// Change sentence
 var span = document.getElementsByTagName("span")[0];
-var texts = [" de payer votre loyer", " aucun anniversaire", " de rendre vos clés"]
+var texts = ["<span class='fade'> de payer votre loyer</span>", "<span class='fade'> aucun anniversaire</span>", "<span class='fade'> de rendre vos clés</span>"]
 
 var showSentences = function() {
   texts.forEach(function(text, index, arr){
     setTimeout(function() {
-      span.style.webkitAnimation = 'none';
-      setTimeout(function() {
-          span.style.webkitAnimation = '';
-      }, 01);
       span.innerHTML = text;
 
       if (index == arr.length - 1) {
@@ -19,3 +17,7 @@ var showSentences = function() {
 };
 
 showSentences();
+
+// Change device
+var device = document.getElementsByClassName("device");
+var images = ["phone-iphone.svg", "phone-android.svg", "phone-muxia.svg"]
