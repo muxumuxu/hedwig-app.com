599d800ca2d3e4de49661ac031949a86de9097fb
Audrey Labuxiere <audreylabuxiere@gmail.com>
2017-05-13 17:01:40 +0200

    added title animation

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,21 @@
 // This is where it all goes :)
-var title = 
+var span = document.getElementsByTagName("span")[0];
+var texts = [" de payer votre loyer", " aucun anniversaire", " de rendre vos clés"]
+
+var showSentences = function() {
+  texts.forEach(function(text, index, arr){
+    setTimeout(function() {
+      span.style.webkitAnimation = 'none';
+      setTimeout(function() {
+          span.style.webkitAnimation = '';
+      }, 01);
+      span.innerHTML = text;
+
+      if (index == arr.length - 1) {
+        setTimeout(showSentences, 5000);
+      }
+    }, index * 5000);
+  });
+};
+
+showSentences();
