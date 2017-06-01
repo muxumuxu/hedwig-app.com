c62901a26e5680c35d5c052dd0994b1ab60a53e0
David Miotti <david@muxumuxu.com>
2017-06-02 00:19:39 +0200

    Added fade effect on bubbles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,41 +1,56 @@
 // This is where it all goes :)
 
+var interval = 7000;
+
 // Change sentence
 var span = document.getElementsByTagName("span")[0];
 var texts = ["<span class='fade'> de payer votre loyer</span>", "<span class='fade'> aucun anniversaire</span>", "<span class='fade'> où sont vos clés</span>"]
 
 var showSentences = function() {
-  texts.forEach(function(text, index, arr){
+  texts.forEach(function(text, index, arr) {
     setTimeout(function() {
       span.innerHTML = text;
 
       if (index == arr.length - 1) {
-        setTimeout(showSentences, 7000);
+        setTimeout(showSentences, interval);
       }
-    }, index * 7000);
+    }, index * interval);
   });
 };
 
 showSentences();
 
-// Change device
-var device = document.getElementById("device");
-var img_classes = ["iphone", "android", "mukia"];
+var iPhoneDevice = document.getElementById("iphone");
+var iPhoneMessage = document.getElementById("iphone_message");
+var androidDevice = document.getElementById("android");
+var androidMessage = document.getElementById("android_message");
+var mukiaDevice = document.getElementById("mukia");
+var mukiaMessage = document.getElementById("mukia_message");
 
-var showDevice = function() {
-  img_classes.forEach(function(img_class, index, arr){
+var devices = [[iPhoneDevice, iPhoneMessage], [androidDevice, androidMessage], [mukiaDevice, mukiaMessage]];
+
+var showDevices = function() {
+  devices.forEach(function(deviceProperties, index, arr) {
     setTimeout(function() {
-      device.classList.add(img_class);
-      device.classList.remove(img_classes[index -1]);
-        if (index == 0) {
-          device.classList.remove("mukia");
-        }
+      var device = deviceProperties[0];
+      var deviceMessage = deviceProperties[1];
 
-      if (index == arr.length - 1) {
-        setTimeout(showDevice, 7000);
+      device.style.display = "block";
+      deviceMessage.classList.add("bubble_fade");
+
+      if (index > 0) {
+        var previousDevice = arr[index - 1][0];
+        previousDevice.style.display = "none";
+      } else {
+        var latestDevice = arr[arr.length - 1][0];
+        latestDevice.style.display = "none";
       }
-    }, index * 7000);
+
+      if (index == arr.length - 1) { // Restart the animation
+        setTimeout(showDevices, interval);
+      }
+    }, index * interval);
   });
 };
 
-showDevice();
+showDevices();
